341ee8e6fe94308c322edc796420907b5374b04a
mehdi <mbdj@free.fr>
2023-09-17 15:43:33 +0200

    task init

---
+++ src/bipolar_stepper_motor_task_package.adb
@@ -0,0 +1,5 @@
+package body Bipolar_Stepper_Motor_Task_package is
+
+   
+
+end Bipolar_Stepper_Motor_Task_package;
---
+++ src/bipolar_stepper_motor_task_package.ads
@@ -0,0 +1,5 @@
+package Bipolar_Stepper_Motor_Task_Package is
+
+   
+
+end Bipolar_Stepper_Motor_Task_package;
